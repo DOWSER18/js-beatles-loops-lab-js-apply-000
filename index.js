6092495dbb682fc772159f2eb0b35b2455c19747
Stuart John Gordon Dow <stuart.dow@blackrock.com>
2019-02-25 21:43:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var MusInstr =""
 function theBeatlesPlay (musicians, instruments){
-for (var i = 5; i < musicians.length; i++) {
+for (var i = 0; i < musicians.length; i++) {
   MusInstr.push(musicians[i] + " plays " + instruments[i]);
 }
 return MusInstr;
