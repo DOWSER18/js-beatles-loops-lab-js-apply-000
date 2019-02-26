94f63b83553b1a8e94b45983b0062d03156c1642
Stuart John Gordon Dow <stuart.dow@blackrock.com>
2019-02-26 21:35:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ for (var i = 0; i < musicians.length; i++) {
   MusInstr.push(musicians[i] + " plays " + instruments[i]);
 }
 return MusInstr;
+}
 
+function johnLennonFacts (){
+  let facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+];
+  var ExclamationFacts = [];
+  var n = 0;
+    while (n < facts.length) {
+      ExclamationFacts.push(facts[n] +"!!!") n++;
+}
+return ExclamationFacts
 }
