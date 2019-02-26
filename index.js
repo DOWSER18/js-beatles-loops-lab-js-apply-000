117b25d40a5249fd265b62bfd660f657aa4b816d
Stuart John Gordon Dow <stuart.dow@blackrock.com>
2019-02-26 21:39:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ return MusInstr;
 
 function johnLennonFacts (){
   var ExclamationFacts = [];
-  let facts = [
-    "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"
-];
+  let facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var i = 0;
     while (i < facts.length) {
       ExclamationFacts.push(facts[i] +"!!!");
